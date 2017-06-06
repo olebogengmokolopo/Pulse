1c967b0572fc8cace59c53785000d926e24e7e84
kevin.gray <kevin.gray@entelect.co.za>
2017-06-06 14:38:00 +0200

    Added sample for disk space sensor

--- Monitor/Modules/components/sampling/samplingController.js
+++ Monitor/Modules/components/sampling/samplingController.js
@@ -13,5 +13,32 @@
 
         /* IMPLEMENTATIONS */
         scope.environment = appEnvironment.environment;
+
+        scope.drives = [
+            {
+                symbol : "C:/",
+                label : "OS Drive",
+                totalSpace : 1234,
+                freeSpace : 456,
+                usedSpace : 778,
+                alertType : "success"
+            },
+            {
+                symbol : "D:/",
+                label : "Applications",
+                totalSpace : 5678,
+                freeSpace : 910,
+                usedSpace : 4768,
+                alertType : "danger"
+            },
+            {
+                symbol : "E:/",
+                label : "Logging",
+                totalSpace : 123,
+                freeSpace : 34,
+                usedSpace : 89,
+                alertType : "warning"
+            }
+        ]
     }
 })();
